957eed65c17de76ed2e9e85d3b197d9dadad41de
azelenin057 <yazel057@gmail.com>
2023-09-25 21:14:09 +0300

    2/3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  body: document.body,
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+
+console.log(refs.btnStop);
+
+refs.btnStart.addEventListener('click', changeColor);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+console.log(getRandomHexColor());
+
+let timerInteval = ''; //Виніс змінну таймеру за функцію, для того щоб мати доступ до нього в різних функціях.(Щоб зупинити його)
+
+function changeColor() {
+  timerInteval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+    refs.btnStart.disabled = true;
+  }, 1000);
+}
+
+function onBtnStop() {
+  clearInterval(timerInteval);
+  refs.btnStart.disabled = false;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,77 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  datetime: document.querySelector('#datetime-picker'),
+  btnStartTimer: document.querySelector('[data-start]'),
+  timerDays: document.querySelector('[data-days]'),
+  timerHours: document.querySelector('[data-hours]'),
+  timerMinutes: document.querySelector('[data-minutes]'),
+  timerSeconds: document.querySelector('[data-seconds]'),
+};
+
+refs.btnStartTimer.disabled = true;
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const currentDate = new Date();
+    if (selectedDates[0].getTime() - currentDate.getTime() > 0) {
+      console.log('Ніхуя себе');
+      refs.btnStartTimer.disabled = false;
+    } else {
+      refs.btnStartTimer.disabled = true;
+      window.alert('Please choose a date in the future');
+    }
+    console.log(selectedDates[0]);
+  },
+};
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+}
+
+function onTimerStart() {
+  const selectedDate = fp.selectedDates[0];
+
+  timerId = setInterval(() => {
+    const startTime = new Date();
+    const countdown = selectedDate - startTime;
+    refs.btnStartTimer.disabled = true;
+
+    if (countdown < 0) {
+      clearInterval(timerId);
+      return;
+    }
+    updateTimerFace(convertMs(countdown));
+  }, 1000);
+}
+
+function updateTimerFace({ days, hours, minutes, seconds }) {
+  refs.timerDays.textContent = addLeadingZero(days);
+  refs.timerHours.textContent = addLeadingZero(hours);
+  refs.timerMinutes.textContent = addLeadingZero(minutes);
+  refs.timerSeconds.textContent = addLeadingZero(seconds);
+}
+
+const fp = flatpickr(refs.datetime, options);
+
+refs.btnStartTimer.addEventListener('click', onTimerStart);
