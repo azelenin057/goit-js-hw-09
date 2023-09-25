a67cd76e3f3eea7551fee70a605c0bd9604de455
azelenin057 <yazel057@gmail.com>
2023-09-25 22:35:41 +0300

    Final

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+const refs = {
+  form: document.querySelector('.form'),
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onCreatePromises(e) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  const dataParams = {};
+
+  for (const [key, value] of formData.entries()) {
+    dataParams[key] = Number(value);
+  }
+
+  let { amount, step, delay } = dataParams;
+
+  for (let i = 1; i <= amount; i += 1) {
+    delay += step;
+    createPromise(i, delay).then(onSuccess).catch(onError);
+
+    refs.form.reset();
   }
 }
+
+function onError({ position, delay }) {
+  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+
+function onSuccess({ position, delay }) {
+  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+refs.form.addEventListener('submit', onCreatePromises);
